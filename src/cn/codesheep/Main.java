69945b54cef69efe8383ac346456c8be8ee7970a
laishumin <1437555011@qq.com>
2022-01-04 16:24:04 +0800

    测试代码

--- src/cn/codesheep/Main.java
+++ src/cn/codesheep/Main.java
@@ -1,10 +1,21 @@
 package cn.codesheep;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String args[]){
-        List<String> a = new ArrayList<>();
+
+//        System.out.println(100.1 * 2);
+//        System.out.println(100.1 * 3);
+//        System.out.println(100.11 * 3);
+//        List<String> list = Arrays.asList("1", "2");
+//        ArrayList<String> list2 = new ArrayList<>();
+
+        LinkedList<String> a = new LinkedList<String>();
+        a.add("1");
+        a.add("sthsrg");
+        for (Iterator iter = a.iterator(); iter.hasNext();){
+            System.out.println("1");
+        }
     }
 }
